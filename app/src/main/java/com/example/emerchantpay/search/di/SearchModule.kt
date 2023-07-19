6289e5daf358fb72577b3106ab6b4e45306d8920
Dimitar Spasov <emerchantpaytestdimitar@gmail.com>
2023-07-20 00:32:14 +0300

    remove unused serch package

--- app/src/main/java/com/example/emerchantpay/search/di/SearchModule.kt
+++
@@ -1,9 +0,0 @@
-package com.example.emerchantpay.search.di
-
-import com.example.emerchantpay.common.di.retrofitModule
-import com.example.emerchantpay.common.di.roomModule
-import org.koin.dsl.module
-
-val searchModule = module {
-    includes(roomModule, retrofitModule)
-}
